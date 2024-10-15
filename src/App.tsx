aa10a669106c4e67767136d3348f7d407656e621
parvaneshojaee <parvane.shojaee1991@gmail.com>
2024-10-15 20:23:04 +0330

    added some classes and ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./assets/global.css";
 import TicTacToe from "./components/TicTacToe";
+import { Toaster } from "./components/ui/toaster";
 
 function App() {
   return (
     <div>
       <TicTacToe />
+      <Toaster />
     </div>
   );
 }
--- src/components/TicTacToe/index.tsx
+++ src/components/TicTacToe/index.tsx
@@ -1,4 +1,14 @@
 import { useEffect, useMemo, useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import GameSVG from "../../../public/assets/images/tic. tac.toe..svg";
+// import WinnerSVG from "../../../public/assets/images/winner-ribbon.png";
+import { useToast } from "@/hooks/use-toast";
 
 enum Players {
   X_PLAYER = "X",
@@ -9,12 +19,16 @@ enum GameStates {
   IN_PROGRESS,
   OVER,
 }
+
 function TicTacToe() {
+  const { toast } = useToast();
+
   const [grid, setGrid] = useState(Array(9).fill(null));
   const [playerTurn, setPlayerTurn] = useState<Players>(Players.X_PLAYER);
   const [gameState, setGameState] = useState<GameStates>(
     GameStates.NOT_STARTED
   );
+//   const [winner, setWinner] = useState<Players | null>(null);
 
   const moveCount = useMemo(
     () => grid.filter((g) => g !== null).length,
@@ -22,37 +36,21 @@ function TicTacToe() {
   );
 
   const move = (index: number) => {
+    if (grid[index] !== null || gameState === GameStates.OVER) return;
+
     setGrid((prev) => {
       const gridCopy = [...prev];
-      if (gridCopy[index] === null) {
-        gridCopy[index] = playerTurn;
-        if (playerTurn === Players.X_PLAYER) {
-          setPlayerTurn(Players.O_PLAYER);
-        } else {
-          setPlayerTurn(Players.X_PLAYER);
-        }
+      gridCopy[index] = playerTurn;
+
+      if (playerTurn === Players.X_PLAYER) {
+        setPlayerTurn(Players.O_PLAYER);
+      } else {
+        setPlayerTurn(Players.X_PLAYER);
       }
+
       return gridCopy;
     });
   };
-
-  useEffect(() => {
-    let timeout: NodeJS.Timeout;
-    timeout = setTimeout(() => {
-      if (
-        playerTurn === Players.O_PLAYER &&
-        gameState !== GameStates.OVER &&
-        moveCount !== 9
-      ) {
-        let randomNum = Math.floor(Math.random() * (9 - 1) + 1);
-        while (grid[randomNum]) {
-          randomNum = Math.floor(Math.random() * (9 - 1) + 1);
-        }
-        move(randomNum);
-      }
-    }, 1000);
-    return () => timeout && clearTimeout(timeout);
-  }, [playerTurn, moveCount, gameState]);
 
   useEffect(() => {
     const lines = [
@@ -65,28 +63,80 @@ function TicTacToe() {
       [0, 4, 8],
       [2, 4, 6],
     ];
+
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (grid[a] && grid[a] === grid[b] && grid[a] === grid[c]) {
-        console.log(grid[a]);
+        // setWinner(grid[a] as Players);
         setGameState(GameStates.OVER);
+        toast({
+          title:
+            Players.X_PLAYER === grid[a] ? "Congrats! You won:)" : "Sorry! You Lost:(",
+        });
+        return;
       }
     }
-  }, [grid]);
+
+    if (moveCount === 9 && gameState !== GameStates.OVER) {
+      setGameState(GameStates.OVER); // It's a draw
+    }
+  }, [grid, moveCount]);
+
+  useEffect(() => {
+    if (gameState !== GameStates.OVER && playerTurn === Players.O_PLAYER) {
+      const timeout = setTimeout(() => {
+        let randomNum = Math.floor(Math.random() * 9);
+        while (grid[randomNum]) {
+          randomNum = Math.floor(Math.random() * 9);
+        }
+        move(randomNum);
+      }, 1000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [playerTurn, grid, gameState]);
+
   return (
-    <div className="flex flex-wrap w-[270px]">
-      {grid.map((item, index) => (
-        <div
-          onClick={() => {
-            playerTurn === Players.X_PLAYER && move(index);
-          }}
-          className="basis-[90px] h-[90px] border-2 border-stone-900"
-        >
-          {item}
-        </div>
-      ))}
-      {gameState === GameStates.OVER && <h1>DONE</h1>}
+    <div className="relative bg-background flex justify-center items-center h-screen">
+      <Card className="bg-card w-[350px]">
+        <CardHeader>
+          <CardTitle className="text-gray-200">{gameState === GameStates.NOT_STARTED && "Let's Play!"}</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-wrap justify-center">
+          {grid.map((item, index) => (
+            <div
+              key={index}
+              onClick={() => {
+                playerTurn === Players.X_PLAYER && move(index);
+              }}
+              className="basis-[90px] h-[90px] bg-background rounded-sm m-1 text-gray-100 flex justify-center items-center text-6xl font-bold"
+            >
+              {item}
+            </div>
+          ))}
+        </CardContent>
+        <CardFooter className="text-orange-500 text-2xl font-bold">
+        {gameState === GameStates.OVER && "Game is Over!"}
+        </CardFooter>
+        {/* {gameState === GameStates.OVER && (
+          <CardFooter>
+            <div className="relative w-full flex justify-center items-center">
+              <img src={WinnerSVG} alt="winner" className="w-[400px]" />
+              <span className="absolute text-gray-200 text-xl font-bold">
+                {winner ? `Winner: Player ${winner}` : "It's a draw!"}
+              </span>
+            </div>
+          </CardFooter>
+        )} */}
+      </Card>
+      <img
+        src={GameSVG}
+        width="20%"
+        alt="img"
+        className="absolute bottom-0 left-0"
+      />
     </div>
   );
 }
+
 export default TicTacToe;
